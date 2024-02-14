ba3e1b00a623e43488604d0c6883c448dab8a493
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2024-02-14 15:15:33 -0500

    add verifyKzgProofBatch and tests

--- src/kzg.ts
+++ src/kzg.ts
@@ -10,7 +10,17 @@ const computeBlobKzgProof = module.cwrap('compute_blob_kzg_proof_wasm', 'string'
 const verifyBlobKzgProof = module.cwrap('verify_blob_kzg_proof_wasm', 'number', ['array', 'array', 'array']) as (blob: Uint8Array, commitment: Uint8Array, proof: Uint8Array) => number
 const verifyKzgProof = module.cwrap('verify_kzg_proof_wasm', 'number', ['array', 'array', 'array','array']) 
 
+const verifyBlobKzgProofBatch = (blobs: Uint8Array[], commitments: Uint8Array[], proofs: Uint8Array[]) => {
+    if (blobs.length !== commitments.length && commitments.length !== proofs.length) {
+        throw new Error('number of blobs, commitments, and proofs, must match')
+    }
+    for (let x = 0; x < blobs.length; x++) {
+        const res = verifyBlobKzgProof(blobs[x], commitments[x], proofs[x])
+        if (res !== 0) return res
+    }
+    return 0
+}
 export const kzg = {
-    loadTrustedSetup, freeTrustedSetup, blobToKzgCommit, computeBlobKzgProof, verifyBlobKzgProof, verifyKzgProof
+    loadTrustedSetup, freeTrustedSetup, blobToKzgCommit, computeBlobKzgProof, verifyBlobKzgProofBatch, verifyKzgProof
 }
 
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -22,7 +22,7 @@ describe('api tests', () => {
         assert.equal(commitment.toLowerCase(), 'ab87358a111c3cd9da8aadf4b414e9f6be5ac83d923fb70d8d27fef1e2690b4cad015b23b8c058881da78a05c62b1173')
         const proof = kzg.computeBlobKzgProof(blob, hexToBytes('0x' + commitment))
         assert.equal(proof.toLowerCase(), '8dd951edb4e0df1779c29d28b835a2cc8b26ebf69a38d7d9afadd0eb8a4cbffd9db1025fd253e91e00a9904f109e81e3')
-        const proofVerified = kzg.verifyBlobKzgProof(blob, hexToBytes('0x' + commitment), hexToBytes('0x' + proof))
+        const proofVerified = kzg.verifyBlobKzgProofBatch([blob], [hexToBytes('0x' + commitment)], [hexToBytes('0x' + proof)])
         assert.equal(proofVerified, 0)
         kzg.freeTrustedSetup()
     })
