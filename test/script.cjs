2d8fbef4fc3925237747dac08c358535f754e2ea
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2024-03-14 14:11:04 -0400

    fix cjs test

--- test/script.cjs
+++ test/script.cjs
@@ -2,7 +2,7 @@ const loadKZG = require('../dist/cjs/index')
 
 const main = async () => {
     const kzg = await loadKZG.loadKZG()
-    const json = require('../src/trustedSetup')
+    const json = require('../dist/cjs/trustedSetup')
     kzg.loadTrustedSetup(json)
     console.log(kzg)
 }
