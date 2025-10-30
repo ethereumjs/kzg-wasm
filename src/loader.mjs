88f1bfeeb7448ad5e5b1f34ffb00ed5dfd90d096
Nathaniel Blakely <nate@innodev.llc>
2025-10-30 06:03:10 -0700

    move url to use await import (#25)

--- src/loader.mjs
+++ src/loader.mjs
@@ -1,12 +1,11 @@
-import { fileURLToPath } from 'url';
-
-    
+  
 export async function loadWasmModule() {
     let wasmBinary = undefined
     if (typeof process !== 'undefined' && typeof process.cwd === 'function') {
         // Dynamically import Node.js modules only when in Node.js environment
         const { resolve, dirname } = await import('path')
         const { readFileSync } = await import('fs')
+        const { fileURLToPath } = await import('url')
         const __filename = fileURLToPath(import.meta.url);
         const __dirname = dirname(__filename);
         const wasmPath = resolve(__dirname, '../wasm/kzg.wasm')
